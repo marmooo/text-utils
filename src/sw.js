5a803080f74f005bbc189e4b395fdc435b1b0090
marmooo <marmooo@users.noreply.github.com>
2022-09-04 16:35:13 +0900

    update

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,8 @@
-var CACHE_NAME = "2022-09-04 15:30";
+var CACHE_NAME = "2022-09-04 16:35";
 var urlsToCache = [
   "/text-utils/",
   "/text-utils/index.js",
   "/text-utils/favicon/favicon.svg",
-  "/text-utils/japanese-numeral.min.js",
   "https://cdn.jsdelivr.net/npm/sprintf-js@1.1.2/src/sprintf.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
 ];
