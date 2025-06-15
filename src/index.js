55c030df89a93e750f1ac2375b49fc98f59c2b9c
marmooo <marmooo@users.noreply.github.com>
2025-06-15 10:44:27 +0900

    bump japanese-numeral from 0.1.16 to 1.0.2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
 import {
   kanji2number,
   number2kanji,
-} from "https://cdn.jsdelivr.net/npm/@geolonia/japanese-numeral@0.1.16/+esm";
+} from "https://cdn.jsdelivr.net/npm/@geolonia/japanese-numeral@1.0.2/+esm";
 import { sprintf } from "https://cdn.jsdelivr.net/npm/sprintf-js@1.1.3/+esm";
 import { WordsNinja } from "./wordsninja.js";
 
